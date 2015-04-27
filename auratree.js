4a2297b4b7f18f9a7be067bd65479a7569f9f574
Rahul Prabhakar <rahul.tejzzz@gmail.com>
2015-04-27 17:08:23 +0530

    Update auratree.js

--- auratree.js
+++ auratree.js
@@ -2,22 +2,23 @@
 app.directive("auratree", function() {
 
 	return {
-		scope : {
-			input : "=",
-			holderid : '@',
-		},
-		template : function(element) {
-			return '<script type="text/ng-template" id="treeTemp">'
-					+ '<li><div ng-include = "holderid"></div>'
-					+ '<ul ng-if="item.children">'
+				scope : {
+					input : "=",
+					holderid : '@',	
+					node : '@'
+				},
+				template : '<script type="text/ng-template" id="treeTemp">'
+					+ '<li>'
+					+'<div ng-include = "holderid"></div>'
+					+ '<ul ng-show="item.children && item.children.length!=0 && item.showChildren">'
 					+ '<li ng-repeat="item in item.children" ng-include="'
 					+ "'treeTemp'"
-					+ '"><div ng-include = "holderid"></div></li>'
-					+ '</ul></li>' + '</script>'
+					+ '"></li>'
+					+ '</ul><div ng-show= "item.children && item.children.length!=0  && !item.showChildren" ng-include = "node" ></div></li>' + '</script>'
 					+ '<ul ng-repeat="item in input" ng-include = "'
-					+ "'treeTemp'" + '"></ul>';
-		},
-		link : function(scope) {
-		}
-	};
+					+ "'treeTemp'" + '"></ul>',
+				link : function(scope) {
+
+				}
+			};
 });
